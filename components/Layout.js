ff4d62899cff33f6a06c1cf0287b477f0d7982c9
aklevecz <teh@raptor.pizza>
2021-09-28 17:20:24 -0700

    Page transitions

--- components/Layout.js
+++ components/Layout.js
@@ -10,7 +10,7 @@ export default function Layout({ children }) {
         <meta name="description" content="Volta Collective" />
         <link rel="icon" href="/favicon.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossrigin />
         <link
           href="https://fonts.googleapis.com/css2?family=Bodoni+Moda:wght@400;500;600;700;800&family=Montserrat:wght@200;300;400;600&display=swap"
           rel="stylesheet"
--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,27 @@ import "../styles/globals.css";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 import Head from "next/head";
+import { AnimatePresence, motion } from "framer-motion";
+
+const pageVariants = {
+  pageInitial: {
+    opacity: 0,
+  },
+  pageAnimate: {
+    backgroundColor: "transparent",
+    opacity: 1,
+  },
+  pageExit: {
+    opacity: 0,
+  },
+};
+
+const pageMotionProps = {
+  initial: "pageInitial",
+  animate: "pageAnimate",
+  exit: "pageExit",
+  variants: pageVariants,
+};
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
@@ -15,13 +36,21 @@ function MyApp({ Component, pageProps }) {
   }, [path]);
   return (
     <>
-      <Head>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0, maximum-scale=1.0;"
-        />
-      </Head>
-      <Component {...pageProps} />
+      <AnimatePresence exitBeforeEnter>
+        <Head>
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0, maximum-scale=1.0;"
+          />
+        </Head>
+        <motion.div
+          key={router.route}
+          {...pageMotionProps}
+          style={{ height: "100%" }}
+        >
+          <Component {...pageProps} />
+        </motion.div>
+      </AnimatePresence>
     </>
   );
 }
