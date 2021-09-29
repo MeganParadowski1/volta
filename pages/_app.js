cec41e8542a8aceffe8f473916726fe61aea718c
aklevecz <teh@raptor.pizza>
2021-09-29 08:45:49 -0700

    Css hack

--- pages/_app.js
+++ pages/_app.js
@@ -4,15 +4,32 @@ import { useEffect } from "react";
 import Head from "next/head";
 import { AnimatePresence, motion } from "framer-motion";
 
-import "../styles/Info.module.css";
-import "../styles/Work.module.css";
+import Router from "next/router";
+
+const routeChange = () => {
+  // Temporary fix to avoid flash of unstyled content
+  // during route transitions. Keep an eye on this
+  // issue and remove this code when resolved:
+  // https://github.com/vercel/next.js/issues/17464
+
+  const tempFix = () => {
+    const allStyleElems = document.querySelectorAll('style[media="x"]');
+    allStyleElems.forEach((elem) => {
+      elem.removeAttribute("media");
+    });
+  };
+  tempFix();
+};
+
+Router.events.on("routeChangeComplete", routeChange);
+Router.events.on("routeChangeStart", routeChange);
 
 const pageVariants = {
   pageInitial: {
     opacity: 0,
   },
   pageAnimate: {
-    backgroundColor: "transparent",
+    // backgroundColor: "transparent",
     opacity: 1,
   },
   pageExit: {
