7a2ce4ccae3f183c63e20f49ce98cd18b4357985
Megan Paradowski <meganparadowski@Megans-MacBook-Pro.local>
2021-10-14 17:49:22 -0700

    Revert firstLoad

--- pages/index.js
+++ pages/index.js
@@ -2,64 +2,23 @@ import Layout from "../components/Layout";
 import styles from "../styles/Home.module.css";
 import { AnimatePresence, motion } from "framer-motion";
 import dynamic from "next/dynamic";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 const GlitchVideo = dynamic(() => import("../components/GlitchVideo"), {
   ssr: false,
 });
 
-let firstLoad = true;
 export default function Home() {
   const [loading, setLoading] = useState(true);
   const doneLoading = () => {
-    if (firstLoad) {
-      setLoading(false);
-      firstLoad = false;
-    }
+    setLoading(false);
   };
-  useEffect(() => {
-    if (firstLoad) {
-      console.log("what");
-      // document.addEventListener("DOMContentLoaded", function () {
-      var lazyVideos = [].slice.call(document.querySelectorAll("video.lazy"));
-      if ("IntersectionObserver" in window) {
-        var lazyVideoObserver = new IntersectionObserver(function (
-          entries,
-          observer
-        ) {
-          entries.forEach(function (video) {
-            if (video.isIntersecting) {
-              for (var source in video.target.children) {
-                var videoSource = video.target.children[source];
-                if (
-                  typeof videoSource.tagName === "string" &&
-                  videoSource.tagName === "SOURCE"
-                ) {
-                  videoSource.src = videoSource.dataset.src;
-                }
-              }
-              video.target.load();
-              video.target.classList.remove("lazy");
-              lazyVideoObserver.unobserve(video.target);
-            }
-          });
-        });
-
-        lazyVideos.forEach(function (lazyVideo) {
-          lazyVideoObserver.observe(lazyVideo);
-        });
-      }
-      // });
-    }
-  }, []);
   return (
     <Layout>
       <AnimatePresence>
-        {loading && firstLoad && (
+        {loading && (
           <motion.div
-            // initial={{ opacity: 1 }}
             exit={{ opacity: 0 }}
-            // animate={{ opacity: 1 }}
             transition={{ duration: 2 }}
             className={styles.loading}
           />
