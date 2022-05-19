9058bd429fe6cf065fe5d6017f0529a7c5e32be8
Megan Paradowski <meganparadowski@Megans-MacBook-Pro.local>
2022-05-18 17:05:48 -0700

    Bite page

---
+++ pages/bite.js
@@ -0,0 +1,17 @@
+import Layout from "../components/Layout";
+import styles from "../styles/Pitch.module.css";
+
+export default function Pitch() {
+  return (
+    <Layout>
+      <div className={styles.container}>
+        <img className={styles.pitch} src="BiteII.jpg" />
+        {/* <div className={styles.subtext}>
+          <a href="https://jeanettarich.cargo.site/About" target="_blank">
+            More about Jeanetta Rich
+          </a>
+        </div> */}
+      </div>
+    </Layout>
+  );
+}
