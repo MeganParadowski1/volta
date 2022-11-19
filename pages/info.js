5769f002f99f760315605299e6863ef3de2e9ba3
megpara <meganparadowski@gmail.com>
2022-11-19 13:01:50 -0800

    Fix info on mobile

--- pages/info.js
+++ pages/info.js
@@ -5,9 +5,9 @@ export default function Info() {
   return (
     <Layout>
       <div className="wrapper">
-        <div className="flex flex-row px-16 items-center">
-          <img src="MM.jpg" className="max-w-xs" />
-          <div className="paragraph pl-8">
+        <div className="md:flex flex-row md:px-16 items-center">
+          <img src="MM.jpg" className="md:max-w-xs" />
+          <div className="paragraph md:pt-0 pt-8 md:pl-8 md:text-left text-justify">
             Directed by Mamie Green and Megan Paradowski, Volta fuses
             physicality, theatricality and multidisciplinary approaches to
             performance. Volta is passionate about presenting dance in
