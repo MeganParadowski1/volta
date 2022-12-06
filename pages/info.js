e9290780d78c377e26421b940f90b8610b6c1482
megpara <meganparadowski@gmail.com>
2022-12-06 09:34:57 -0800

    Change info photo

--- pages/info.js
+++ pages/info.js
@@ -6,7 +6,7 @@ export default function Info() {
     <Layout>
       <div className="wrapper">
         <div className="md:flex flex-row md:px-16 items-center">
-          <img src="MM.jpg" className="md:max-w-xs" />
+          <img src="volta_headshot.jpg" className="lg:max-w-md md:max-w-xs" />
           <div className="paragraph md:pt-0 pt-8 md:pl-8 md:text-left text-justify">
             Directed by Mamie Green and Megan Paradowski, Volta fuses
             physicality, theatricality and multidisciplinary approaches to
