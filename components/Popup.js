c376499fd66ea12580c127ff8deb371ef1d7d866
megpara <meganparadowski@gmail.com>
2022-12-07 16:19:51 -0800

    Make popup text smaller

--- components/Popup.js
+++ components/Popup.js
@@ -29,11 +29,11 @@ export default function Popup({ open, closePopup }) {
             <div className="accentFont uppercase text-center text-5xl pb-4">
               Volta Co-Lab
             </div>
-            <div className="pb-4">
+            <div className="pb-4 text-sm">
               A workshop series that celebrates inclusive and innovative
               dance-making.
             </div>
-            <div className="pb-4">
+            <div className="pb-6 text-sm">
               Saturday, January 14th | 12-4pm |{" "}
               <div className="inline underline underline-offset-4 decoration-1">
                 <a href="https://brockusprojectdance.org/space" target="_blank">
